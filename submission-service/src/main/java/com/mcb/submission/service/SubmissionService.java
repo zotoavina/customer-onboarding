e21abd6684e4d3eda9dadf6574c9e692e41e9159
Zotoavina <zotoavinanantenaina@gmail.com>
2024-06-30 18:29:53 +0200

    Submission serv : create submission service and implements save method

---
+++ submission-service/src/main/java/com/mcb/submission/service/SubmissionService.java
@@ -0,0 +1,18 @@
+package com.mcb.submission.service;
+
+import com.mcb.submission.persistence.entity.CustomerApplication;
+import lombok.NonNull;
+
+import java.util.Optional;
+
+public interface SubmissionService {
+
+    @NonNull
+    Optional<CustomerApplication> findApplicationByApplicationId(String applicationUUID);
+
+    @NonNull
+    CustomerApplication validateAndSaveApplication(@NonNull CustomerApplication customerApplication);
+
+    @NonNull
+    CustomerApplication findApplicationByApplicationIdOrElseThrow(String applicationUUID);
+}
---
+++ submission-service/src/main/java/com/mcb/submission/service/impl/SubmissionServiceImpl.java
@@ -0,0 +1,61 @@
+package com.mcb.submission.service.impl;
+
+import com.mcb.submission.persistence.entity.CustomerApplication;
+import com.mcb.submission.persistence.repository.SubmissionRepository;
+import com.mcb.submission.service.DataRefApiService;
+import com.mcb.submission.service.SubmissionService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Slf4j
+@Service
+public class SubmissionServiceImpl implements SubmissionService {
+
+    private final SubmissionRepository submissionRepository;
+
+    private final DataRefApiService dataRefApiService;
+    private final Validator validator;
+
+    public SubmissionServiceImpl(SubmissionRepository submissionRepository,
+                                 DataRefApiService dataRefApiService,
+                                 Validator validator) {
+        this.submissionRepository = submissionRepository;
+        this.dataRefApiService = dataRefApiService;
+        this.validator = validator;
+    }
+
+    @Override
+    public @NonNull CustomerApplication validateAndSaveApplication(@NonNull CustomerApplication customerApplication) {
+        log.info("Saving customer application: {}", customerApplication.getApplicationId());
+        Set<ConstraintViolation<CustomerApplication>> violations = validator.validate(customerApplication);
+
+        if (!violations.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            for (ConstraintViolation<CustomerApplication> violation : violations) {
+                sb.append(violation.getMessage()).append("\n");
+            }
+            throw new ConstraintViolationException("Validation failed: " + sb, violations);
+        }
+        dataRefApiService.checkDataRef(customerApplication);
+        return submissionRepository.save(customerApplication);
+    }
+
+    @Override
+    public @NonNull Optional<CustomerApplication> findApplicationByApplicationId(String applicationUUID) {
+        return submissionRepository.findCustomerApplicationByApplicationId(applicationUUID);
+    }
+
+    @Override
+    public @NonNull CustomerApplication findApplicationByApplicationIdOrElseThrow(String applicationUUID) {
+        return findApplicationByApplicationId(applicationUUID)
+                .orElseThrow(() -> new EntityNotFoundException("No application with uuid identifier " + applicationUUID));
+    }
+}
