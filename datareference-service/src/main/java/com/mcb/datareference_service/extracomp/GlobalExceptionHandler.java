1dcfffc87dec8c00c2c1eeacd888180ad2ffb099
Zotoavina <zotoavinanantenaina@gmail.com>
2024-06-30 07:00:08 +0200

    Create data ref service for check data ref values, add test

---
+++ datareference-service/src/main/java/com/mcb/datareference_service/extracomp/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.mcb.datareference_service.extracomp;
+
+import com.mcb.datareference_service.dto.ResponseFormatDto;
+import com.mcb.datareference_service.exception.ApiCallException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.security.InvalidParameterException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = LogManager.getLogger(GlobalExceptionHandler.class);
+
+    /**
+     * Handle exception due to the failure while validating dto
+     *
+     * @param e
+     * @return
+     */
+    @ExceptionHandler({MethodArgumentNotValidException.class, InvalidParameterException.class})
+    public ResponseEntity<ResponseFormatDto> handleValidationException(Exception e) {
+        LOGGER.warn("Argument validation exception : due to error {}", e.getMessage());
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        Object errorData = null;
+        String errorMessage = "";
+        if (e instanceof MethodArgumentNotValidException me) {
+            Map<String, String> errors = new HashMap<>();
+            me.getBindingResult()
+                    .getFieldErrors()
+                    .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+            errorData = errors;
+            errorMessage = "Invalid argument for the action";
+        }
+        if (e instanceof InvalidParameterException ie) {
+            errorMessage = ie.getMessage();
+        }
+        return ResponseFormatDto.buildResponse(errorData, status, errorMessage);
+    }
+
+
+    @ExceptionHandler(ApiCallException.class)
+    public ResponseEntity<ResponseFormatDto> handleApiCallException(Exception e) {
+        log.error("Api call exception {}", e.getMessage());
+        return ResponseFormatDto.buildResponse(null, HttpStatus.BAD_REQUEST,
+                "Error while calling third party api");
+    }
+
+}
---
+++ datareference-service/src/test/java/com/mcb/datareference_service/service/impl/DataRefServiceImplTest.java
@@ -0,0 +1,83 @@
+package com.mcb.datareference_service.service.impl;
+
+import com.mcb.datareference_service.dto.Country;
+import com.mcb.datareference_service.dto.DataCheckRequestDto;
+import com.mcb.datareference_service.dto.DataCheckRespDto;
+import com.mcb.datareference_service.persistence.entity.ApplyingPurpose;
+import com.mcb.datareference_service.persistence.entity.BusinessActivity;
+import com.mcb.datareference_service.persistence.entity.EntityType;
+import com.mcb.datareference_service.service.ApplyingPurposeService;
+import com.mcb.datareference_service.service.BusinessActivityService;
+import com.mcb.datareference_service.service.CountryService;
+import com.mcb.datareference_service.service.EntityTypeService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DataRefServiceImplTest {
+
+    @InjectMocks
+    private DataRefServiceImpl refService;
+
+    @Mock
+    private CountryService countryService;
+    @Mock
+    private EntityTypeService typeService;
+    @Mock
+    private BusinessActivityService activityService;
+
+    @Mock
+    private ApplyingPurposeService purposeService;
+
+    @Test
+    void checkDataRefUsingValidParam() {
+        // arrange
+        DataCheckRequestDto reqDto = new DataCheckRequestDto();
+        reqDto.setCountryName("England");
+        reqDto.setActivityUuid(UUID.randomUUID().toString());
+        reqDto.setPurposeUuid(UUID.randomUUID().toString());
+        reqDto.setEntityTypeUuid(UUID.randomUUID().toString());
+
+        var country = new Country("England", "eng");
+
+        var entityType = new EntityType();
+        entityType.setId(1);
+        entityType.setName("Association");
+
+        var purpose = new ApplyingPurpose();
+        var activity = new BusinessActivity();
+
+        when(typeService.getEntityTypeByUuidOrElseThrow(anyString()))
+                .thenReturn(entityType);
+
+        when(activityService.checkAndGetUsingUuid(anyString()))
+                .thenReturn(activity);
+        when(purposeService.getApplyingPurposeByUuidOrElseThrow(anyString()))
+                .thenReturn(purpose);
+
+        when(countryService.findCountryByNameOrElseThrow(anyString()))
+                .thenReturn(country);
+
+        // act
+        DataCheckRespDto respDto = refService.checkDataRef(reqDto);
+
+        // assert
+        assertAll(
+                () -> assertNotNull(respDto),
+                () -> assertEquals("England", respDto.getCountry().name()),
+                () -> assertNotNull(respDto.getActivity()),
+                () -> assertNotNull(respDto.getEntityType()),
+                () -> assertNotNull(respDto.getPurpose())
+        );
+    }
+
+}
