7c18c618502e6c4688b6e65e0190bef63074874e
Zotoavina <zotoavinanantenaina@gmail.com>
2024-06-30 20:22:21 +0200

    Submission serv : define interface for submission management

--- submission-service/src/main/java/com/mcb/submission/service/SubmissionManagementService.java
+++ submission-service/src/main/java/com/mcb/submission/service/SubmissionManagementService.java
@@ -1,5 +1,22 @@
 package com.mcb.submission.service;
 
+import com.mcb.submission.persistence.entity.ApplicationStatus;
+import com.mcb.submission.persistence.entity.CustomerApplication;
+
+import java.util.List;
+
 public interface SubmissionManagementService {
+
+    List<CustomerApplication> getListOfSubmissionBasedOnStatus(ApplicationStatus status);
+
+    List<CustomerApplication> getListOfSubmissionBasedOnStatus(String status);
+
+    CustomerApplication updateCustomerApplication(CustomerApplication application);
+
+    void proceedApplication(String applicationId);
+
+    void rejectApplication(String applicationId);
+
+    void approveApplication(String applicationId);
 
 }
