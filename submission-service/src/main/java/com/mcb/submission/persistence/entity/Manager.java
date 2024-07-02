867404d0d854068850ca45377cc7756ce8d60cd1
Zotoavina <zotoavinanantenaina@gmail.com>
2024-07-02 08:24:41 +0200

    Submission serv : implement loadby username method for manager service (for security purpose)

--- submission-service/src/main/java/com/mcb/submission/persistence/entity/Manager.java
+++ submission-service/src/main/java/com/mcb/submission/persistence/entity/Manager.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -32,7 +33,7 @@ public class Manager implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority(profile.getProfileCode()));
     }
 
     @Override
